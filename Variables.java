bc0bfcb94b51e95dd570bfbfc712a88387a70cdd
NewToCoding26 <liridon992@gmx.de>
2024-09-19 09:47:04 +0200

    Variable added

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 55;
+        boolean b = true;
+        double c = 5.43;
+        String s = "Haloo";
+
+        System.out.println(a+c);
+    }
+}
