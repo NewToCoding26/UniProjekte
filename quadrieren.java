d65e938ef0f4f6d2b81714d3aefcdd9f3b56a796
NewToCoding26 <liridon992@gmx.de>
2024-09-19 10:19:05 +0200

    added quadrieren

---
+++ quadrieren.java
@@ -0,0 +1,9 @@
+public class quadrieren {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        int quadrat = x * x;
+
+        System.out.println("Das Quadrat von " + x + " ist: " + quadrat);
+    }
+}
+
