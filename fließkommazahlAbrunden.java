dac55b13f204be46f5a1815326ebe6cbe792fab2
NewToCoding26 <liridon992@gmx.de>
2024-09-19 10:37:09 +0200

    jgdh

---
+++ fließkommazahlAbrunden.java
@@ -0,0 +1,7 @@
+public class fließkommazahlAbrunden {
+    public static void main(String[] args) {
+        double x = 9.525;
+        double abrunden = Math.floor(x);
+        System.out.println("Die abgerundete Zahl ist:" + abrunden);
+    }
+}
