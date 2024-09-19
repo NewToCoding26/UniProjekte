1184f16db961e30ff10c8174f560ecefb3bcee53
NewToCoding26 <liridon992@gmx.de>
2024-09-19 11:03:59 +0200

    commited werteberich

---
+++ werteberich.java
@@ -0,0 +1,7 @@
+public class werteberich {
+    public static void main(String[] args) {
+        int x = 2147483647;
+        int sum = x + 1;
+        System.out.println("Die zahl ist: " +sum);
+    }
+}
