7fd6121015cdec9bc747cc91d885c64d7aef6d7e
NewToCoding26 <liridon992@gmx.de>
2024-09-19 16:09:50 +0200

    commitedt wahrheitsWerte

---
+++ wahrheitsWerte.java
@@ -0,0 +1,10 @@
+public class wahrheitsWerte {
+    public static void main(String[] args) {
+        boolean h = 3<1;
+        boolean k = 4+2 == 6;
+        boolean l = Math.pow(2, 2) == 3 || Math.pow(2, 2) == 4;
+        System.out.println("3 < 1 ist: " + h);
+        System.out.println("4 + 2 == 6 ist: " + k);
+        System.out.println("2^2 == 3 oder 2^2 == 4 ist: " + l);
+    }
+}
