033110124176cdc297e61c11183b0dbbc8df92e4
NewToCoding26 <liridon992@gmx.de>
2024-09-22 11:32:03 +0200

    commited zweiArgumenteEinWort

---
+++ zweiArgumenteEinWort.java
@@ -0,0 +1,9 @@
+public class zweiArgumenteEinWort {
+    public static void main(String[] args) {
+        int ganzeZahl = 44;
+        double gleitZahl = 0.44;
+        String wort = "Decke";
+
+        System.out.println("" + ganzeZahl + " + 5 = " + (ganzeZahl + 5) + ", " + gleitZahl + " / 3 = " + (gleitZahl / 3) + ", Wort: " + wort + ".");
+    }
+}
